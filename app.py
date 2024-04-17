4d4ec2a84b0c7c05a872fc1ea3f73759952a03d2
Abhay Singh <87566721+itz-abhay@users.noreply.github.com>
2024-04-17 21:32:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
+from PyPDF2 import PdfFileReader
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from streamlit import session_state
